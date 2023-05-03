e1c3c7aa8aea6ad4f4a5d332a904ede590e67027
Kwaku Owusu-Tieku <kowusu01@gmail.com>
2023-05-03 16:31:13 -0400

    Update malaria_data_processor.R

--- file-readers/malaria/malaria_data_processor.R
+++ file-readers/malaria/malaria_data_processor.R
@@ -10,7 +10,7 @@ fnLoadExploreAndSaveStats <- function(data_file_path, db_connection){
   # - mismatch 
   # 
 
-  data_set <- fnReadFile(fdata_file_path)
+  data_set <- fnReadFile(data_file_path)
   
   # take a look at what the result looks like
   #glimpse(data_set)
