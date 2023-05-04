442961995e065a57b604bc1374239f6b3148af55
Kwaku Owusu-Tieku <kowusu01@gmail.com>
2023-05-03 20:55:22 -0400

    update data folder for v2

--- file-readers/setup/clean_up.sql
+++ file-readers/setup/clean_up.sql
@@ -2,7 +2,7 @@
 -- connect to the database, i.e. set the current database to the one just created:
 \connect malariadb_dev;
 
-set AUTOCOMMIT ON;
+set AUTOCOMMIT on
 
 delete from data_issues_details;
 delete from cases_reported_bad;
